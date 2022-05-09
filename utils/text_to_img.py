20ec46444878bf84e78b037aa5167e46def73c45
Syaro <91415593+NekoRabi@users.noreply.github.com>
2022-05-10 02:51:19 +0800

    图文转换更新

--- utils/text_to_img.py
+++ utils/text_to_img.py
@@ -1,10 +1,12 @@
+from io import BytesIO
 from typing import Iterable
 
 from PIL import Image, ImageDraw, ImageFont
 
 
-def whitebgk_blacktext(path: str,text: Iterable = None, fontsize: int = 20,
-                       bold: bool = False):
+def text_to_image(path: str, text: Iterable = None, fontsize: int = 20,
+                       bold: bool = False, fontcolor: tuple = (0, 0, 0), bgkcolor=(255, 255, 255),
+                       backimgpath: str = None, imgbytes=None):
     textlength = 0
     texts = []
     if bold:
@@ -14,21 +16,25 @@ def whitebgk_blacktext(path: str,text: Iterable = None, fontsize: int = 20,
         font = ImageFont.truetype(
             font='./data/fonts/MiSans-Light.ttf', size=fontsize)
     if type(text) == str:
-        texts = text.replace('\t','   ').split('\n')
+        texts = text.replace('\t', '   ').split('\n')
     elif type(text) == dict:
         for k, v in text.items():
-            texts.append(f'{k}:{v}'.replace('\t','   ').strip())
+            texts.append(f'{k}:{v}'.replace('\t', '   ').strip())
     elif type(text) in [list, set, tuple]:
         for item in text:
-            texts.append(f'{item}'.replace('\t','   ').strip())
+            texts.append(f'{item}'.replace('\t', '   ').strip())
     for t in texts:
         if len(t.strip()) > textlength:
             textlength = len(t)
-
-    bgimg = Image.new('RGB', ((textlength + 2) * fontsize, (len(texts) + 2) * (fontsize + 5)), (255, 255, 255))
+    if backimgpath:
+        bgimg = Image.open(f'{backimgpath}').convert("RGB")
+    elif imgbytes:
+        bgimg = Image.open(BytesIO(imgbytes)).convert("RGB")
+    else:
+        bgimg = Image.new('RGB', ((textlength + 2) * fontsize, (len(texts) + 2) * (fontsize + 5)), bgkcolor)
     textdraw = ImageDraw.Draw(bgimg)
     for i in range(len(texts)):
         textdraw.text((fontsize, i * (fontsize + 5) + fontsize), text=f'{texts[i].strip()}',
-                      font=font, fill=(0, 0, 0))
+                      font=font, fill=fontcolor)
     bgimg.save(f'./images/{path}')
     return
