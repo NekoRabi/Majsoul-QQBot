72cbf9c892dd3b5ce6d95bc8f98dbe6883b9de77
Syaro <91415593+NekoRabi@users.noreply.github.com>
2022-05-20 23:33:11 +0800

    修复了文本转图片排版不正确的问题，现在图片右边不会出现一大截空白了

--- plugin/ImgOperation/bwimg.py
+++ plugin/ImgOperation/bwimg.py
@@ -22,10 +22,10 @@ def makebwimg(imgname, text: str = ""):
         fontsize = width // textlength
     font = ImageFont.truetype(
         font='./plugin/ImgOperation/MiSans-Bold.ttf', size=fontsize)
-    bgimg = Image.new('RGB', (width, height + 20 + fontsize), (0, 0, 0))
+    bgimg = Image.new('RGB', (width, height + 30 + fontsize), (0, 0, 0))
     bgimg.paste(img, (0, 0, width, height))
     textdraw = ImageDraw.Draw(bgimg)
-    textdraw.text(((width - fontsize * textlength) / 2, height + 10), text=f'{text}', font=font, fill=(255,255,255))
+    textdraw.text(((width - font.getsize(text)[0]) / 2, height + 10), text=f'{text}', font=font, fill=(255,255,255))
     bgimg.save(f'./images/tempimg/{filename}{os.path.splitext(imgname)[-1]}')
 
 
--- utils/text_to_img.py
+++ utils/text_to_img.py
@@ -5,8 +5,8 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 def text_to_image(path: str, text: Iterable = None, fontsize: int = 20,
-                       bold: bool = False, fontcolor: tuple = (0, 0, 0), bgkcolor=(255, 255, 255),
-                       backimgpath: str = None, imgbytes=None):
+                  bold: bool = False, fontcolor: tuple = (0, 0, 0), bgkcolor=(255, 255, 255),
+                  backimgpath: str = None, imgbytes=None):
     textlength = 0
     texts = []
     if bold:
@@ -26,12 +26,17 @@ def text_to_image(path: str, text: Iterable = None, fontsize: int = 20,
     for t in texts:
         if len(t.strip()) > textlength:
             textlength = len(t)
+    maxwidth = fontsize
+    for item in texts:
+        wd = font.getsize(item)[0]
+        if wd > maxwidth:
+            maxwidth = wd
     if backimgpath:
         bgimg = Image.open(f'{backimgpath}').convert("RGB")
     elif imgbytes:
         bgimg = Image.open(BytesIO(imgbytes)).convert("RGB")
     else:
-        bgimg = Image.new('RGB', ((textlength + 2) * fontsize, (len(texts) + 2) * (fontsize + 5)), bgkcolor)
+        bgimg = Image.new('RGB', (maxwidth+ 2 * (fontsize+5), (len(texts) + 2) * (fontsize + 5)), bgkcolor)
     textdraw = ImageDraw.Draw(bgimg)
     for i in range(len(texts)):
         textdraw.text((fontsize, i * (fontsize + 5) + fontsize), text=f'{texts[i].strip()}',
